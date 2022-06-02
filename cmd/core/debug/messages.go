54d506f61a1f544e5d2d86bbd99c8340967a4d92
MkWilp-boot <joaocwb22@protonmail.com>
2022-06-02 20:45:25 -0300

    new builtin fun "length_of"

--- cmd/core/debug/messages.go
+++ cmd/core/debug/messages.go
@@ -4,6 +4,7 @@ const (
 	DefaultRuntimeException = "fatal error while executing program"
 	InvalidBoolType         = "non bool type used as bool"
 	//OFMException            = "Out of memory in %q storage\"
-	InvalidTypeForIndex = "can not use a non %q as index"
-	InvalidInstruction  = "invalid instruction of type '%d'"
+	InvalidTypeForIndex       = "can not use a non %q as index"
+	InvalidInstruction        = "invalid instruction of type '%d'"
+	InvalidTypeForInstruction = "invalid type of '%s' for instruction of type '%s'"
 )
--- cmd/pkg/helpers/functions/bultin.go
+++ cmd/pkg/helpers/functions/bultin.go
@@ -1,17 +1,33 @@
 package functions
 
 import (
+	"fmt"
+	"t/cmd/core/debug"
 	"t/cmd/pkg/helpers"
 	orthtypes "t/cmd/pkg/types"
 )
 
-var Functions map[string]func(*[]orthtypes.Operand)
+var Functions map[string]func(stack *[]orthtypes.Operand)
 
 func init() {
 	Functions = make(map[string]func(*[]orthtypes.Operand))
+
 	Functions["to_string"] = func(stack *[]orthtypes.Operand) {
-		o1 := helpers.StackPop(&(*stack))
+		o1 := helpers.StackPop(&*stack)
 		res := ToString(o1)
 		*stack = append(*stack, res)
+	}
+
+	Functions["length_of"] = func(stack *[]orthtypes.Operand) {
+		o1 := helpers.StackPop(&*stack)
+		switch o1.VarType {
+		case orthtypes.PrimitiveSTR:
+			*stack = append(*stack, orthtypes.Operand{
+				VarType: orthtypes.PrimitiveI32,
+				Operand: fmt.Sprint(len(o1.Operand)),
+			})
+		default:
+			panic(fmt.Errorf(debug.InvalidTypeForInstruction, o1.VarType, "Functions[length_of]"))
+		}
 	}
 }
